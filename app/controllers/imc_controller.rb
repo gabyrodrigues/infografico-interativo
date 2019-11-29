d6c2ce992beba3bbcabe54a4f8750b15aedd7504
Gabriely <gabyrlima.99@gmail.com>
2019-11-29 16:34:21 -0300

    teste parametros

--- app/controllers/imc_controller.rb
+++ app/controllers/imc_controller.rb
@@ -1,4 +1,13 @@
 class ImcController < ApplicationController
 	def index
 	end
+
+	def formulario
+		render formulario_imc_index_path
+	end
+
+	def calcular
+		raise params[:banana]
+		render formulario_imc_index_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :imc
+  resources :imc do
+  	collection do
+        match 'formulario', via: [:get, :post]
+        match 'calcular', via: [:get, :post]
+    end
+  end
   root to: 'imc#index'
 end
